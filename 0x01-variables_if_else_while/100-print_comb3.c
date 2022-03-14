93b8f53cea9de43467197c02105d2e782ab9422b
Vincent-ondeng <vincentondeng@gmail.com>
2022-03-13 22:17:23 -0700

    print comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,19 @@ int main(void)
 {
 	int i, j, k;
 
-	i=0;
+	i =0;
 
 	while (i < 100)
 	{
-		j= i % 10;
-		k= i / 10;
+		j = i % 10;
+		k = i / 10;
 
 		if (k < j)
 		{
 			putchar(k + '0');
 			putchar(j + '0');
 
-			if (i < 89 )
+			if (i < 89)
 			{
 				putchar(44);
 				putchar(32);
