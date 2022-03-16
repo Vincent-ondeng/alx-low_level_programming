c11cb69592aaba26bb8aafca11ef998956274aeb
Vincent-ondeng <vincentondeng@gmail.com>
2022-03-15 18:24:23 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	int lastDig;
+	int last_dig;
 
-	if (n >= 0)
+	if (n < 0)
 	{
-		lastDig; = (n % 10);
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
 	else
 	{
-		lastDig = (n & 10) * -1;
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
-	_putchar('0' + lastDig);
-	return (lastDig);
 }
