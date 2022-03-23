62871acecf1737bec8c3ab05b13f2721b32c40ae
Vincent-ondeng <vincentondeng@gmail.com>
2022-03-23 14:47:33 -0700

    more functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,8 +8,6 @@
 void print_triangle(int size)
 
 {
-
-
 	int i;
 	int z;
 	int d;
@@ -22,25 +20,18 @@ void print_triangle(int size)
 		{
 			for (z = d; z > 0; z--)
 			{
-				_putchar (' ');
+				_putchar(' ');
 			}
-
 			for (p = 0; p <= i; p++)
 			{
-				_putchar (35);
+				_putchar(35);
 			}
-
 			d--;
-
-			_putchar ('\n');
-
-
+			_putchar('\n');
 		}
-
 	}
-	
 	else
 	{
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
