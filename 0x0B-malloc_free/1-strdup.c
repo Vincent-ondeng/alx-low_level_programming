204e01e8f03a4264c737e04a50cd65a5b36b7163
Vincent-ondeng <vincentondeng@gmail.com>
2022-04-09 05:09:34 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -31,7 +31,7 @@ char *_strcpy(char *dest, char *src)
 
 	while (src[i] != '\0')
 	{
-		dest[i] - src[i];
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
@@ -57,9 +57,9 @@ char *_strdup(char *str)
 
 	size = _strlen(str) + 1;
 
-	dest = (char *) malloc(size * sizeof(char));
+	dst = (char *) malloc(size * sizeof(char));
 
-	if (dst == 0);
+	if (dst == 0)
 	{
 		return (NULL);
 	}
