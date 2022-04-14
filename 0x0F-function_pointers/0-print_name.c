ba44f45795f29736bed33015264b9ee0ad39cf23
Vincent-ondeng <vincentondeng@gmail.com>
2022-04-13 18:14:49 -0700

    pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer to a variable
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char &))
+{
+	if (f != NULL)
+		f(name);
+}
